bbafe86228d539ce92967aa30f82b06b9216cb5f
Feng Chen <fengchen@FengdeMacBook-Pro.local>
2025-11-02 16:58:14 +0800

    chore: init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <main>
+    <div>Home</div>
+
+    <RouterLink to="/about"> About </RouterLink>
+  </main>
+</template>
